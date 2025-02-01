711713d58a76464c9a97fe59c2f347c2f20b53b9
ofirgalcon <ofir@gal.co.uk>
2025-02-01 17:18:57 +0000

    Update ard_tab.php
    add listing and report buttons
    optimise via clause 3.5

--- views/ard_tab.php
+++ views/ard_tab.php
@@ -1,4 +1,15 @@
 
+
+<div id="lister" style="font-size: large; float: right;">
+    <a href="/show/listing/ard/ard" title="List">
+        <i class="btn btn-default tab-btn fa fa-list"></i>
+    </a>
+</div>
+<div id="report_btn" style="font-size: large; float: right;">
+    <a href="/show/report/ard/ard_report" title="Report">
+        <i class="btn btn-default tab-btn fa fa-th"></i>
+    </a>
+</div>
 <h2 data-i18n="ard.ard"></h2>
 <div id="ard-tab"></div>
 
@@ -6,132 +17,85 @@
 
 <script>
 $(document).on('appReady', function(){
+    var $ardMsg = $('#ard-msg');
+    var $ardTab = $('#ard-tab');
+
     $.getJSON(appUrl + '/module/ard/get_tab_data/' + serialNumber, function(data){
         
-        if( ! data ){
-            $('#ard-msg').text(i18n.t('no_data'));
+        if (!data) {
+            $ardMsg.text(i18n.t('no_data'));
         } else {
-            
-            // Hide
-            $('#ard-msg').text('');
+            $ardMsg.text('');
             $('#ard-count-view').removeClass('hide');
 
-            var skipThese = ['id', 'serial_number','admin_machines'];
-            $.each(data, function(i,d){
+            var skipThese = ['id', 'serial_number', 'admin_machines'];
+            var rows = '';
+            var administratorsRows = '';
+            var taskServersRows = '';
 
-                // Generate rows from data
-                var rows = ''
-                var administrators_rows = ''
-                var task_servers_rows = ''
-                for (var prop in d){
-                    // Skip skipThese
-                    if(skipThese.indexOf(prop) == -1){
-                        if (d[prop] == '' || d[prop] == null || d[prop] == "{}"){
-                        // Do nothing for empty values to blank them
+            $.each(data, function(i, d){
+                for (var prop in d) {
+                    if (skipThese.indexOf(prop) !== -1 || d[prop] === '' || d[prop] === null || d[prop] === "{}") {
+                        continue;
+                    }
 
-                        } else if((prop == 'screensharing_request_permission' || prop == 'load_menu_extra' || prop == 'console_allows_remote' || prop == 'allow_all_local_users' || prop == 'directory_login') && (d[prop] == "yes" || d[prop] == 1)){
-                           rows = rows + '<tr><th>'+i18n.t('ard.'+prop)+'</th><td>'+i18n.t('yes')+'</td></tr>';
-                        }
-                        else if((prop == 'screensharing_request_permission' || prop == 'load_menu_extra' || prop == 'console_allows_remote' || prop == 'allow_all_local_users' || prop == 'directory_login') && (d[prop] == "no" || d[prop] == 0)){
-                           rows = rows + '<tr><th>'+i18n.t('ard.'+prop)+'</th><td>'+i18n.t('no')+'</td></tr>';
-                        }
-                        
-                        else if((prop == 'vnc_enabled') && (d[prop] == "yes" || d[prop] == 1)){
-                           rows = rows + '<tr><th>'+i18n.t('ard.'+prop)+'</th><td>'+i18n.t('enabled')+'</td></tr>';
-                        }
-                        else if((prop == 'vnc_enabled') && (d[prop] == "no" || d[prop] == 0)){
-                           rows = rows + '<tr><th>'+i18n.t('ard.'+prop)+'</th><td>'+i18n.t('disabled')+'</td></tr>';
-                        }
-                        
-                        // Else if build out the administrators table
-                        else if(prop == "administrators"){
-                            var administrators_data = JSON.parse(d['administrators']);
-                            administrators_rows = '<tr><th style="max-width: 140px;">'+i18n.t('ard.computername')+'</th><th style="max-width: 85px;">'+i18n.t('ard.has_latest_reporting_info')+'</th><th style="max-width: 60px;">'+i18n.t('ard.mac_address')+'</th><th style="max-width: 300px;">'+i18n.t('ard.task_server_id')+'</th><th style="max-width: 300px;">'+i18n.t('ard.ip_address')+'</th><th style="max-width: 300px;">'+i18n.t('ard.port')+'</th><th style="max-width: 300px;">'+i18n.t('ard.last_contact')+'</th></tr>'
-                            $.each(administrators_data, function(i,d){
-                                if (typeof d['HasLatestReportingInfo'] !== "undefined" && d['HasLatestReportingInfo'] == 1) {
-                                        var has_latest_reporting_info = i18n.t('yes')
-                                    } else if (typeof d['HasLatestReportingInfo'] !== "undefined" && d['HasLatestReportingInfo'] == 0) {
-                                        var has_latest_reporting_info = i18n.t('yes')
-                                    } else {
-                                        var has_latest_reporting_info = ""
-                                    }
-                                if (typeof d['LastContact'] !== "undefined") {
-                                        var last_contact = d['LastContact']
-                                    } else {
-                                        var last_contact = ""
-                                    }
-                                
-                                if (typeof d['LastContact'] !== "undefined") {var date_1 = new Date(d['LastContact'] * 1000); last_contact = '<span title="'+moment(date_1).fromNow()+'">'+moment(date_1).format('llll')} else {var last_contact = ""}
-                                if (typeof d['ComputerName'] !== "undefined") {var ard_name = d['ComputerName']} else {var ard_name = ""}
-                                if (typeof d['MAC_address'] !== "undefined") {var mac_address = d['MAC_address']} else {var mac_address = ""}
-                                if (typeof d['TaskServerID'] !== "undefined") {var task_server_id = d['TaskServerID']} else {var task_server_id = ""}
-                                if (typeof d['IPAddress'] !== "undefined") {var ip_address = d['IPAddress']} else {var ip_address = ""}
-                                if (typeof d['Port'] !== "undefined") {var port = d['Port']} else {var port = ""}
-                                // Generate rows from data
-                                administrators_rows = administrators_rows + '<tr><td>'+ard_name+'</td><td>'+has_latest_reporting_info+'</td><td>'+mac_address+'</td><td>'+task_server_id+'</td><td>'+ip_address+'</td><td>'+port+'</td><td>'+last_contact+'</td></tr>';
-                            })
-                            administrators_rows = administrators_rows // Close administrators table framework
-                        }
-                        
-                        // Else if build out the task servers table
-                        else if(prop == "task_servers"){
-                            var task_servers_data = JSON.parse(d['task_servers']);
-                            task_servers_rows = '<tr><th style="max-width: 240px;">'+i18n.t('ard.dns_name')+'</th><th style="max-width: 60px;">'+i18n.t('ard.mac_address')+'</th><th style="max-width: 300px;">'+i18n.t('ard.ip_address')+'</th><th style="max-width: 300px;">'+i18n.t('ard.port')+'</th></tr>'
-                            $.each(task_servers_data, function(i,d){
-                                if (typeof d['DNSName'] !== "undefined") {var dns_name = d['DNSName']} else {var dns_name = ""}
-                                if (typeof d['MAC_address'] !== "undefined") {var mac_address = d['MAC_address']} else {var mac_address = ""}
-                                if (typeof d['IPAddress'] !== "undefined") {var ip_address = d['IPAddress']} else {var ip_address = ""}
-                                if (typeof d['Port'] !== "undefined") {var port = d['Port']} else {var port = ""}
-                                // Generate rows from data
-                                task_servers_rows = task_servers_rows + '<tr><td>'+dns_name+'</td><td>'+mac_address+'</td><td>'+ip_address+'</td><td>'+port+'</td></tr>';
-                            })
-                            task_servers_rows = task_servers_rows // Close task servers table framework
-                        }
+                    var value = d[prop];
+                    var translatedProp = i18n.t('ard.' + prop);
 
-                        else {
-                        rows = rows + '<tr><th style="width: 165px;">'+i18n.t('ard.'+prop)+'</th><td style="max-width: 500px;">'+d[prop]+'</td></tr>';
-                        }
+                    if (['screensharing_request_permission', 'load_menu_extra', 'console_allows_remote', 'allow_all_local_users', 'directory_login'].includes(prop)) {
+                        rows += '<tr><th>' + translatedProp + '</th><td>' + i18n.t(value == "yes" || value == 1 ? 'yes' : 'no') + '</td></tr>';
+                    } else if (prop === 'vnc_enabled') {
+                        rows += '<tr><th>' + translatedProp + '</th><td>' + i18n.t(value == "yes" || value == 1 ? 'enabled' : 'disabled') + '</td></tr>';
+                    } else if (prop === 'administrators') {
+                        administratorsRows = buildAdministratorsTable(JSON.parse(value));
+                    } else if (prop === 'task_servers') {
+                        taskServersRows = buildTaskServersTable(JSON.parse(value));
+                    } else {
+                        rows += '<tr><th style="width: 165px;">' + translatedProp + '</th><td style="max-width: 500px;">' + value + '</td></tr>';
                     }
                 }
-                
-                $('#ard-tab')
-                    .append($('<div>')
-                        .append($('<table style="width: 500px;">')
-                            .addClass('table table-striped table-condensed')
-                            .append($('<tbody>')
-                                .append(rows))))
-                
-                // Only draw the administrators table if there is something in it
-                if (administrators_rows !== ""){
-                    $('#ard-tab')
-                        .append($('<h4>')
-                            .append(" "+i18n.t('ard.administrators')))
-                        .append($('<div>')
-                            .append($('<table style="width: 985px;">')
-                                .addClass('table table-striped table-condensed')
-                                .append($('<tbody>')
-                                    .append(administrators_rows))))
-                } else {
-                    $('#ard-tab')
-                        .append($('<br>'))
-                }
-                
-                // Only draw the task_servers table if there is something in it
-                if (task_servers_rows !== ""){
-                    $('#ard-tab')
-                        .append($('<h4>')
-                            .append(" "+i18n.t('ard.task_servers')))
-                        .append($('<div>')
-                            .append($('<table style="width: 600px;">')
-                                .addClass('table table-striped table-condensed')
-                                .append($('<tbody>')
-                                    .append(task_servers_rows))))
-                } else {
-                    $('#ard-tab')
-                        .append($('<br>'))
-                }
-            })
+            });
+
+            $ardTab.append(createTable(rows, 500));
+
+            if (administratorsRows) {
+                $ardTab.append('<h4>' + i18n.t('ard.administrators') + '</h4>')
+                       .append(createTable(administratorsRows, 985));
+            } else {
+                $ardTab.append('<br>');
+            }
+
+            if (taskServersRows) {
+                $ardTab.append('<h4>' + i18n.t('ard.task_servers') + '</h4>')
+                       .append(createTable(taskServersRows, 600));
+            } else {
+                $ardTab.append('<br>');
+            }
         }
     });
+
+    function createTable(rows, width) {
+        return $('<div>').append($('<table style="width: ' + width + 'px;">')
+            .addClass('table table-striped table-condensed')
+            .append($('<tbody>').append(rows)));
+    }
+
+    function buildAdministratorsTable(data) {
+        var rows = '<tr><th style="max-width: 140px;">' + i18n.t('ard.computername') + '</th><th style="max-width: 85px;">' + i18n.t('ard.has_latest_reporting_info') + '</th><th style="max-width: 60px;">' + i18n.t('ard.mac_address') + '</th><th style="max-width: 300px;">' + i18n.t('ard.task_server_id') + '</th><th style="max-width: 300px;">' + i18n.t('ard.ip_address') + '</th><th style="max-width: 300px;">' + i18n.t('ard.port') + '</th><th style="max-width: 300px;">' + i18n.t('ard.last_contact') + '</th></tr>';
+        $.each(data, function(i, d) {
+            var hasLatestReportingInfo = d['HasLatestReportingInfo'] == 1 ? i18n.t('yes') : i18n.t('no');
+            var lastContact = d['LastContact'] ? '<span title="' + moment(new Date(d['LastContact'] * 1000)).fromNow() + '">' + moment(new Date(d['LastContact'] * 1000)).format('llll') + '</span>' : '';
+            rows += '<tr><td>' + (d['ComputerName'] || '') + '</td><td>' + hasLatestReportingInfo + '</td><td>' + (d['MAC_address'] || '') + '</td><td>' + (d['TaskServerID'] || '') + '</td><td>' + (d['IPAddress'] || '') + '</td><td>' + (d['Port'] || '') + '</td><td>' + lastContact + '</td></tr>';
+        });
+        return rows;
+    }
+
+    function buildTaskServersTable(data) {
+        var rows = '<tr><th style="max-width: 240px;">' + i18n.t('ard.dns_name') + '</th><th style="max-width: 60px;">' + i18n.t('ard.mac_address') + '</th><th style="max-width: 300px;">' + i18n.t('ard.ip_address') + '</th><th style="max-width: 300px;">' + i18n.t('ard.port') + '</th></tr>';
+        $.each(data, function(i, d) {
+            rows += '<tr><td>' + (d['DNSName'] || '') + '</td><td>' + (d['MAC_address'] || '') + '</td><td>' + (d['IPAddress'] || '') + '</td><td>' + (d['Port'] || '') + '</td></tr>';
+        });
+        return rows;
+    }
 });
 </script>
